Fixed bug in batch normalization.

scripts/deep_mlp_model.py
@@ -71,7 +71,9 @@ class DeepMlpModel(DeepNNModel):
         inputs = tf.divide(inputs - tf.tile(self._center,[num_unrollings]),
                              tf.tile(self._scale,[num_unrollings]))
         targets = tf.divide(targets - self._center, self._scale)
-        
+
+      self._t = targets
+      
       if config.input_dropout is True: inputs = self._input_dropout(inputs)
 
       num_prev = total_input_size
@@ -110,8 +112,10 @@ class DeepMlpModel(DeepNNModel):
         optimizer = getattr(tf.train, config.optimizer)(learning_rate=self._lr,**args)
       else:
         raise RuntimeError("Unknown optimizer = %s"%config.optimizer)
-
-      self._train_op = optimizer.apply_gradients(zip(grads, tvars))
+     
+      update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+      with tf.control_dependencies(update_ops):
+        self._train_op = optimizer.apply_gradients(zip(grads, tvars))
 
   def _input_dropout(self,inputs):
     # This implementation of dropout dropouts an entire feature along the time dim
@@ -129,11 +133,11 @@ class DeepMlpModel(DeepNNModel):
   def _batch_relu_layer(self, x, size, phase, scope):
     with tf.variable_scope(scope):
       h1 = tf.contrib.layers.fully_connected(x, size,
-                                              activation_fn=None,
-                                              scope='dense')
+                                             # activation_fn=None,
+                                             scope='dense')
       h2 = tf.contrib.layers.batch_norm(h1, 
                                         center=True, scale=True,
                                         is_training=phase,
                                         scope='bn')
-      return tf.nn.relu(h2, 'relu')
+      return h2 # tf.nn.relu(h2, 'relu')
 

scripts/deep_nn_model.py
@@ -65,7 +65,7 @@ class DeepNNModel(object):
 
      return mse
 
-  def test_step(self, sess, batch, keep_prob=1.0):
+  def test_step(self, sess, batch, training=False):
     """
     Take one step through the data set. A step contains a sequences of batches
     where the sequence is of size num_unrollings. The batches are size
@@ -83,12 +83,16 @@ class DeepNNModel(object):
       valid_evals:
     """
 
-    feed_dict = self._get_feed_dict(batch,keep_prob=1.0,training=False)
+    feed_dict = self._get_feed_dict(batch,keep_prob=1.0,training=training)
+    
+    (x,y,z) = sess.run([self._predictions,self._t,self._mse],feed_dict)
 
-    (x,y,z) = sess.run([self._predictions,self._targets,self._mse],feed_dict)
-    print(x)
+    np.set_printoptions(suppress=True)
+    np.set_printoptions(precision=3)
+
+    print(np.array(x))
     print("---------------")
-    print(y)
+    print(np.array(y))
     print("---------------")
     print(z)
     print("---------------")
@@ -115,9 +119,6 @@ class DeepNNModel(object):
   def set_scaling_params(self,session,center=None,scale=None):
     assert(center is not None)
     assert(scale is not None)
-
-    # wide_center = np.tile(center,self._num_unrollings)
-    # wide_scale = np.tile(scale,self._num_unrollings)
     session.run(tf.assign(self._center,center))
     session.run(tf.assign(self._scale,scale))
     