4742a93b114a05af2ce96dcf638023fdc3199fcc
John Alberg <john.alberg@euclidean.com>
2017-10-28 22:59:53 -0700

    cleaning up repo

---
+++ exprmnts/window-iter-train-paper/scripts/run-iter-train-rnn-window.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+./scripts/run-iter-train.sh -c config/iter-rnn.conf -t train-rnn-window -w 25 -s 2000 -e 2017 -p yes
+
+
---
+++ exprmnts/window-iter-train-paper/scripts/run-iter-train-rnn.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+./scripts/run-iter-train.sh -c config/iter-rnn.conf -t train-rnn -w 0 -s 2000 -e 2017 -p yes
+
+
