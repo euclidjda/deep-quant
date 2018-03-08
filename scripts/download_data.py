0d8bf4279035d80a03867d1224dde211bcf18475
John Alberg <alberg@euclidean.com>
2018-03-08 13:48:12 -0500

    Added date to remote file names so they can be versioned

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -34,43 +34,38 @@ data_dir   = 'datasets'
 s3_bucket  = 'deep-quant-data'
 
 # we should read this list from file in datasets
-remote_files = ['open-dataset.dat.gz']
+remote_file = 'open-dataset-2018-03-08.dat.gz'
+local_file  = 'open-dataset.dat'
 
 def maybe_download(directory, filename, url):
-  """Download filename from url unless it's already in directory."""
-  if not os.path.exists(directory):
-    print("Creating directory %s" % directory)
-    os.mkdir(directory)
-  filepath = os.path.join(directory, filename)
-  if not os.path.exists(filepath):
-    print("Downloading %s" % (url+filename))
-    wget.download(url+filename, out=directory)
-    statinfo = os.stat(filepath)
-    print("\nSuccesfully downloaded", filename, statinfo.st_size, "bytes")
-  else:
-    print("file %s already exists in %s"% (filename,directory) )
-  return filepath
+    """Download filename from url unless it's already in directory."""
+    if not os.path.exists(directory):
+        print("Creating directory %s" % directory)
+        os.mkdir(directory)
+    filepath = os.path.join(directory, filename)
+    if not os.path.exists(filepath):
+        print("Downloading %s" % (url+filename))
+        wget.download(url+filename, out=directory)
+        statinfo = os.stat(filepath)
+        print("\nSuccesfully downloaded", filename, statinfo.st_size, "bytes")
+    else:
+        print("file %s already exists in %s"% (filename,directory) )
+    return filepath
 
 def gunzip_file(gz_path, new_path):
-  """Unzips from gz_path into new_path."""
-  if not os.path.exists(new_path):
-    print("Unpacking %s to %s" % (gz_path, new_path))
-    with gzip.open(gz_path, "rb") as gz_file:
-      with open(new_path, "wb") as new_file:
-        for line in gz_file:
-          new_file.write(line)
-
+    """Unzips from gz_path into new_path."""
+    if not os.path.exists(new_path):
+        print("Unpacking %s to %s" % (gz_path, new_path))
+        with gzip.open(gz_path, "rb") as gz_file:
+            with open(new_path, "wb") as new_file:
+                for line in gz_file:
+                    new_file.write(line)
 
 def download_data():
-
-    local_files  = [ os.path.splitext(remote_files[i])[0]
-                         for i in range(len(remote_files)) ]
-
     print("Downloading data ...")
-    for i in range(len(remote_files)):
-        maybe_download(data_dir, remote_files[i], data_url)
-        gunzip_file(data_dir+'/'+remote_files[i],
-                    data_dir+'/'+local_files[i])
+    maybe_download(data_dir, remote_file, data_url)
+    gunzip_file(data_dir+'/'+remote_file,
+                data_dir+'/'+local_file)
 
 def main():
     download_data()
